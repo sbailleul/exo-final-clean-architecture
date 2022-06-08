f45b79b4027587e98124d28e3b96801129eba162
sbailleul <44065690+sbailleul@users.noreply.github.com>
2022-06-08 22:09:39 +0200

    [chore]: Fix git error

---
+++ TaskManger.Presentation.CLI/CommandParser.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console.Cli;
+using TaskManger.Presentation.CLI.Commands;
+
+namespace TaskManger.Presentation.CLI;
+
+public class CommandParser
+{
+    private readonly CommandApp _app;
+
+    public CommandParser()
+    {
+        _app = new CommandApp();
+        Configure();
+
+    }
+    private void Configure()
+    {
+        _app.Configure(config =>
+        {
+            config.AddCommand<ListTasksCommand>("list");
+            config.AddCommand<AddTaskCommand>("add");
+        });
+    }
+
+    public int Run(string[] args)
+    {
+        return _app.Run(args);
+    }
+}
---
+++ TaskManger.Presentation.CLI/Commands/ListTasksCommand.cs
@@ -0,0 +1,9 @@
+﻿using Spectre.Console.Cli;
+
+public class ListTasksCommand : Command
+{
+    public override int Execute(CommandContext context)
+    {
+        return 0;
+    }
+}
