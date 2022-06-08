e27641c160adc09f33e95d4821dbc56192104ded
sbailleul <44065690+sbailleul@users.noreply.github.com>
2022-06-08 18:10:55 +0200

    [chore]: Init skeleton

---
+++ TaskManager.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace TaskManager.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
